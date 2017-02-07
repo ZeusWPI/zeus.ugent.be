44e9a4f76c527c21a7d7bc066a38f7cae2f21a72
Lorin Werthen <werthenlorin@gmail.com>
2017-02-08 00:51:46 +0100

    minor code cleanup

--- lib/helpers/tile_helper.rb
+++ lib/helpers/tile_helper.rb
@@ -1,14 +1,7 @@
-
 module TileHelper
   def get_teaser(post)
-    content = strip_html(post.compiled_content)
-    return truncate(content, 300)
+    # excerptize is part of the TextHelper, given by nanoc
+    # https://nanoc.ws/doc/reference/helpers/#text
+    excerptize(post.reps[:text].compiled_content, length: 300)
   end
-
-  def truncate(s, max=70, elided = ' ...')
-    s.match( /(.{1,#{max}})(?:\s|\z)/ )[1].tap do |res|
-      res << elided unless res.length == s.length
-    end
-  end
-
 end
