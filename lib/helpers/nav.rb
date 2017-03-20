63377b6d1c6c0273d87eb70c4e41484fe41805ae
Lorin Werthen <werthenlorin@gmail.com>
2017-03-20 15:15:14 +0100

    bulma 0.4 n shit

--- lib/helpers/nav.rb
+++ lib/helpers/nav.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
+# Helpers for navbar partial
 module NavigationHelper
   def nav_items
     navigables.each do |item|
       # Kind of hacky way to check if page is child of another page
-      root = %r{/[^\/]*/}.match(item.path)[0]
+      root = %r{/.+?/}.match(item.path)[0]
 
       is_active = @item_rep && @item_rep.path.start_with?(root)
 
@@ -10,6 +13,9 @@ module NavigationHelper
     end
   end
 
+  # Returns every navigable item
+  # A navigable item contains the :navigable attribute
+  # Optionally contains an order attribute, determining the order in the navbar
   def navigables
     items.select { |i| i[:navigable] }.sort_by { |x| x[:order] || 10_000 }
   end
