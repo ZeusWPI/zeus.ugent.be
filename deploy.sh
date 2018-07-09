6cae6410deaa3dde314af9752b6ae231f20d78e6
lorin <werthenlorin@gmail.com>
2018-07-09 18:46:16 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,6 @@ if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
     bundle exec nanoc --env=prod deploy public
   fi
 else
-  rsync -aglpPrtvz --delete output/ "deploy@$TRAVIS_PULL_REQUEST.zeus.werthen.com:/var/www/html/$TRAVIS_PULL_REQUEST/"
+  # TODO: Re-enable this when wildcard certs are okay for zeus.gent or zeuswpi.org
+  # rsync -aglpPrtvz --delete output/ "deploy@$TRAVIS_PULL_REQUEST.zeus.werthen.com:/var/www/html/$TRAVIS_PULL_REQUEST/"
 fi
