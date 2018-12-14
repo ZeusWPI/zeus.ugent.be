233477e937325cb281c4e1c7bccdd95308b47a30
fklinck <francisklinck@gmail.com>
2018-12-14 01:48:29 +0100

    Converted cammie.coffee to cammie.js

---
+++ content/assets/scripts/cammie.js
@@ -0,0 +1,42 @@
+var debounce;
+
+debounce = function(func, wait, initial) {
+  var timeout;
+  if (initial == null) {
+    initial = function() {};
+  }
+  timeout = false;
+  return function() {
+    var args, callNow, context, later;
+    context = this;
+    args = arguments;
+    later = function() {
+      timeout = null;
+      return func.apply(context, args);
+    };
+    callNow = !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) {
+      return initial.apply(context, args);
+    }
+  };
+};
+
+$("#cammie-ctrls").mousemove(debounce(function() {
+  return $(this).removeClass('display');
+}, 3000, function() {
+  return $(this).addClass('display');
+}));
+
+$('.ctrl').click(function() {
+  var $context;
+  $context = $(this);
+  return $.ajax("https://kelder.zeus.ugent.be/webcam/cgi/ptdc.cgi", {
+    data: {
+      command: $context.data('command'),
+      posX: $context.data('x'),
+      posY: $context.data('y')
+    }
+  });
+});
