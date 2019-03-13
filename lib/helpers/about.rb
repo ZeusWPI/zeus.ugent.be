224f9bbf3fa7a4bafbb3883ca2f0ff9abaad9644
Robbe Van Herck <robbevanherck1@gmail.com>
2019-03-13 21:12:03 +0100

    Added dummy sponsoring members

--- lib/helpers/about.rb
+++ lib/helpers/about.rb
@@ -6,4 +6,12 @@ module AboutHelper
   def current_bestuur
     bestuur_of(@config[:academic_year].to_sym)
   end
+
+  def sponsoring_members_of(year)
+    data_from(:supporting_members)[year]
+  end
+
+  def current_sponsoring_members
+    sponsoring_members_of(@config[:academic_year].to_sym)
+  end
 end
