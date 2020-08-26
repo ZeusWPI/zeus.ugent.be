c1a90db8715b6fb09c06324b65ca58b4f5468cd0
Lorin Werthen <werthenlorin@gmail.com>
2020-08-26 10:11:45 +0200

    speed up compilation of html pages in production

--- lib/filters/htmlpress.rb
+++ lib/filters/htmlpress.rb
@@ -1,5 +1,14 @@
 require 'htmlcompressor'
+require 'rainpress'
+require 'uglifier'
+
+class UglifyProxy < Uglifier
+  alias_method :compress, :compile
+end
+
+JS_COMPRESSOR = UglifyProxy.new
+CSS_COMPRESSOR = Rainpress
 
 Nanoc::Filter.define(:html_press) do |content, options|
-  HtmlCompressor::Compressor.new(compress_javascript: true, compress_css: true, remove_quotes: true, simple_boolean_attributes: true).compress content
+  HtmlCompressor::Compressor.new(compress_javascript: true, javascript_compressor: JS_COMPRESSOR, compress_css: true, css_compressor: CSS_COMPRESSOR, remove_quotes: true, simple_boolean_attributes: true).compress content
 end
