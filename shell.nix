ff3abf60f84fad7fa28bd6b622aa1d6de816d863
lorin <werthenlorin@gmail.com>
2021-03-21 12:09:39 +0100

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -3,6 +3,7 @@ let
 in with pkgs; mkShell {
   nativeBuildInputs = [
     ruby bundler libxml2 nodejs yarn cacert git glibcLocales
+    pandoc (texlive.combine { inherit (texlive) scheme-basic xetex unicode-math enumitem booktabs; })
   ];
   shellHook = ''
     export LANG=en_US.UTF-8
