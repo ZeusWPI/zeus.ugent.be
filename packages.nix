71949a023e267c0f2767a6c140f80eb25c24cab2
Lorin Werthen <werthenlorin@gmail.com>
2021-10-22 12:32:24 +0200

    try to cache nix deps when building

---
+++ packages.nix
@@ -0,0 +1,15 @@
+let
+  rev = "70904d4a9927a4d6e05c72c4aaac4370e05107f3";
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+  pkgs = import nixpkgs {};
+  ruby = pkgs.ruby;
+  gems = pkgs.bundlerEnv {
+    name = "zeus.ugent.be";
+    inherit ruby;
+    gemdir = ./.;
+  };
+in with pkgs;
+  [
+    ruby_3_0 gems bundler libxml2 nodejs yarn cacert git glibcLocales
+    pandoc (texlive.combine { inherit (texlive) scheme-basic xetex unicode-math enumitem booktabs ulem; })
+  ] ++ (if stdenv.isDarwin then [terminal-notifier] else [])
--- shell.nix
+++ shell.nix
@@ -1,15 +1,11 @@
-with (import <nixpkgs> {});
 let
-  gems = pkgs.bundlerEnv {
-    name = "zeus.ugent.be";
-    inherit ruby;
-    gemdir = ./.;
-  };
-in mkShell {
-  nativeBuildInputs = [
-    ruby_3_0 gems bundler libxml2 nodejs yarn cacert git glibcLocales
-    pandoc (texlive.combine { inherit (texlive) scheme-basic xetex unicode-math enumitem booktabs ulem; })
-  ] ++ (if stdenv.isDarwin then [terminal-notifier] else []);
+  build-deps = import ./packages.nix;
+  rev = "70904d4a9927a4d6e05c72c4aaac4370e05107f3";
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+  pkgs = import nixpkgs {};
+in
+pkgs.mkShell {
+  nativeBuildInputs = build-deps;
   shellHook = ''
     export LANG=en_US.UTF-8
   '';
