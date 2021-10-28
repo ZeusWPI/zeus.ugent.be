61c680f6c879a29b29f8cc27ee8aeaa8a7196f5d
maartenvn <maartenvn.eu@gmail.com>
2021-10-28 21:33:20 +0200

    feat(cammie): improve behavior of chat form

--- content/assets/scripts/message.js
+++ content/assets/scripts/message.js
@@ -1,16 +1,31 @@
-$('.send').click(function() {
-  var $context;
-  $context = $(this);
+const form = $("#cammieForm");
+const formMessage = form.find('input[name="message"]');
+const formButton = form.find('button[type="submit"]');
+const responseError = $("#cammieFormResponseError");
+const responseSuccess = $("#cammieFormResponseSuccess");
+
+$("#cammieForm").submit((e) => {
+  e.preventDefault();
+  formButton.addClass("is-loading");
+
   return $.ajax({
     url: "https://kelder.zeus.ugent.be/messages/",
     contentType: "text/plain",
     type: "POST",
-    data: $('.chatbox').val(),
-    success: function() {
-      return $('#chat-response').text('Success! :)');
+    data: form.find('input[name="message"]').val(),
+    success() {
+      responseError.addClass("is-hidden");
+      responseSuccess.removeClass("is-hidden");
+      responseSuccess.text("Message has been successfully send.");
+      formMessage.val("");
     },
-    error: function() {
-      return $('#chat-response').text('Error !1!');
-    }
+    error() {
+      responseSuccess.addClass("is-hidden");
+      responseError.removeClass("is-hidden");
+      responseError.text("Something went wrong, please try again later.");
+    },
+    complete() {
+      formButton.removeClass("is-loading");
+    },
   });
 });
