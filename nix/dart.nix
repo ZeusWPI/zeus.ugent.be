b3b9898825a3523461759a23cb054d57fe3c672a
Lorin Werthen <werthenlorin@gmail.com>
2021-11-02 12:39:07 +0100

    try and fix

--- nix/dart.nix
+++ nix/dart.nix
@@ -1,34 +1,27 @@
-{ pkgs ? import <nixpkgs> { }
-, sha256 ? "0d8ks6x4a32v906p0xb5nn0hkivyi6gr6nyiq10sz1zxx8gkcl7v"
-, version ? "1.43.4" }:
-
-let
-   unstable = import (builtins.fetchTarball {
+{ pkgs ? (import (builtins.fetchTarball {
       # Descriptive name to make the store path easier to identify
       name = "nixos-unstable-2021-11-02";
       # Commit hash for nixos-unstable as of 2018-09-12
       url = "https://github.com/nixos/nixpkgs/archive/1380230a378043ecd55c3d50a0bfa1f401d80efc.tar.gz";
       # Hash obtained using `nix-prefetch-url --unpack <url>`
       sha256 = "062ix9661gq7ysgs4a2wppspvxdn2wcaf0l2pfiwpg8jkl3rgfnb";
-    }) {};
-in with pkgs;
-stdenv.mkDerivation rec {
+    }) {})
+, sha256 ? "0d8ks6x4a32v906p0xb5nn0hkivyi6gr6nyiq10sz1zxx8gkcl7v"
+, version ? "1.43.4" }:
+with pkgs;
+stdenv.mkDerivation {
     name = "dart-sass-${version}";
     inherit version;
     
-    isExecutable = true;
-
     src = fetchurl {
         inherit sha256;
         url = "https://github.com/sass/dart-sass/archive/${version}.tar.gz";
     };
 
-    phases = "unpackPhase installPhase";
-
     installPhase = ''
         export HOME=$PWD
-        ${unstable.dart}/bin/dart pub get
-        ${unstable.dart}/bin/dart compile exe -Dversion=${version} bin/sass.dart -o sass
+        ${dart}/bin/dart pub get
+        ${dart}/bin/dart compile exe -Dversion=${version} bin/sass.dart -o sass
         cp -r . $out
         ln -s $out/sass $out/bin/sass
     '';
