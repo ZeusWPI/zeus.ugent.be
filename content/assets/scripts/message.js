2c022fd8bb94a7670161bcd186a36c08e5eb0d75
Xander <xander@biltopia.org>
2023-10-04 16:30:22 +0200

    send a message to kelder using socket test

--- content/assets/scripts/message.js
+++ content/assets/scripts/message.js
@@ -5,28 +5,25 @@ const formButton = form.find('button[type="submit"]');
 const responseError = $("#cammieFormResponseError");
 const responseSuccess = $("#cammieFormResponseSuccess");
 
+const socket = io("https://kelder.zeus.ugent.be");
+
+socket.on('replymessage', function(obj) {
+    alert("Kelder says: " + obj['message']);
+})
+
 $("#cammieForm").submit((e) => {
   e.preventDefault();
   formButton.addClass("is-loading");
-  return $.ajax({
-    url: "https://kelder.zeus.ugent.be/messages/",
-    contentType: "text/plain",
-    type: "POST",
-    headers: formName.val() ? { "X-Username": formName.val()} : {},
-    data: formMessage.val(),
-    success() {
+  socket.timeout(4000).emit("message",{username: formName.val(), message: formMessage.val()},(err,_) => {
+    if (err) {
+      responseSuccess.addClass("is-hidden");
+      responseError.removeClass("is-hidden");
+      responseError.text("Something went wrong, please try again later.");
+    } else {
       responseError.addClass("is-hidden");
       responseSuccess.removeClass("is-hidden");
       responseSuccess.text("Message has been successfully sent.");
       formMessage.val("");
-    },
-    error() {
-      responseSuccess.addClass("is-hidden");
-      responseError.removeClass("is-hidden");
-      responseError.text("Something went wrong, please try again later.");
-    },
-    complete() {
-      formButton.removeClass("is-loading");
-    },
-  });
+    }
+  })
 });
