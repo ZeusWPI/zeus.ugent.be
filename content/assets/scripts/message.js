b0d5acb82d25642895249e86c6379a18a4a769d4
vincent <vincent@vallaeys.com>
2026-02-19 16:23:46 +0100

    chore(cammie): use plain websockets

--- content/assets/scripts/message.js
+++ content/assets/scripts/message.js
@@ -1,50 +1,69 @@
 const form = $("#cammieForm");
-const formName = $('#cammieFormName');
-const formMessage = $('#cammieFormMessage');
+const formName = $("#cammieFormName");
+const formMessage = $("#cammieFormMessage");
 const formButton = form.find('button[type="submit"]');
 const responseError = $("#cammieFormResponseError");
 const responseSuccess = $("#cammieFormResponseSuccess");
 const modal = $("#cammieFormModal");
 const modalName = $("#cammieModalName");
-const modalCancel = $("#cammieModalCancel");
 
-const socket = io("https://kelder.zeus.ugent.be");
+const socket = new WebSocket("ws://kelder.zeus.ugent.be/ws/messages");
 
-socket.on('replymessage', function(obj) {
-    alert("Kelder says: " + obj['message']);
-})
+socket.onmessage = function (ev) {
+  let frame;
+  try {
+    frame = JSON.parse(ev.data);
+  } catch {
+    return;
+  }
 
-$("#cammieForm").submit((e) => {
+  if (frame.event === "replymessage") {
+    const obj = frame.data || {};
+    const user = obj.name ? obj.name : "Kelder";
+    alert(`${user} says: ${obj.message}`);
+  }
+
+  if (frame.event === "ack") {
+    responseError.addClass("is-hidden");
+    responseSuccess.removeClass("is-hidden");
+    responseSuccess.text("Message has been successfully sent.");
+    formMessage.val("");
+    formButton.removeClass("is-loading");
+  }
+
+  if (frame.event === "error") {
+    responseSuccess.addClass("is-hidden");
+    responseError.removeClass("is-hidden");
+    responseError.text(frame?.data?.message || "Something went wrong.");
+    formButton.removeClass("is-loading");
+  }
+};
+
+form.on("submit", function (e) {
   e.preventDefault();
   formButton.addClass("is-loading");
-  
-  if (!formName.val() || formName.val() === "") {
+
+  if (!formName.val()) {
     modal.addClass("is-active");
-    return
-  }
-  
-  socket.timeout(3000).emit("message",{username: formName.val(), message: formMessage.val()},(err,_) => {
-    if (err) {
-      responseSuccess.addClass("is-hidden");
-      responseError.removeClass("is-hidden");
-      responseError.text("Something went wrong, please try again later.");
-    } else {
-      responseError.addClass("is-hidden");
-      responseSuccess.removeClass("is-hidden");
-      responseSuccess.text("Message has been successfully sent.");
-      formMessage.val("");
-    }
     formButton.removeClass("is-loading");
-  })
+    return;
+  }
+
+  socket.send(JSON.stringify({
+    event: "message",
+    data: {
+      username: formName.val(),
+      message: formMessage.val()
+    }
+  }));
 });
 
-$("#cammieModalSend").on("click", e => {
+$("#cammieModalSend").on("click", function () {
   modal.removeClass("is-active");
   formName.val(modalName.val() || "-");
-  modal.removeClass("is-active");
   form.submit();
 });
 
-$("#cammieModalCancel").on("click", e => {
+$("#cammieModalCancel").on("click", function () {
   modal.removeClass("is-active");
 });
