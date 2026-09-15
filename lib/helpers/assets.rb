e2b8d2a454a86c974cbc2f66aeb7761c77642418
Lander De Geeter <lander.degeeter@ugent.be>
2026-09-15 18:53:38 +0200

    localize the asset paths

--- lib/helpers/assets.rb
+++ lib/helpers/assets.rb
@@ -1,9 +1,9 @@
 module AssetHelper
   def asset(type, identifier)
     {
-      img: "<img src='/assets/images/#{identifier}' alt='#{identifier}' />",
-      js: "<script src='/assets/scripts/#{identifier}.js'></script>",
-      css: "<link rel='stylesheet' type='text/css' href='/assets/stylesheets/#{identifier}.css'>"
+      img: "<img src='#{localized_path("/assets/images/#{identifier}")}' alt='#{identifier}' />",
+      js: "<script src='#{localized_path("/assets/scripts/#{identifier}.js")}'></script>",
+      css: "<link rel='stylesheet' type='text/css' href='#{localized_path("/assets/stylesheets/#{identifier}.css")}'>"
     }[type]
   end
 
