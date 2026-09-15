10f081ec727801a8377298ce8663e59a33082f19
Lander De Geeter <lander.degeeter@ugent.be>
2026-09-15 18:53:12 +0200

    add helper functions to switch locales and localize paths

--- lib/helpers/i18n.rb
+++ lib/helpers/i18n.rb
@@ -1,9 +1,33 @@
 module I18nHelper
+  def current_locale
+    ENV.fetch('SITE_LOCALE', 'nl').to_sym
+  end
+
+  def localized_path(path)
+    path = path.to_s
+    return path unless path.start_with?('/')
+
+    # Nanoc paths do not include the locale because both locale builds share
+    # the same source tree; add it only when rendering a site link.
+    return path if path.match?(%r{^/(?:en|nl)(?:/|$)})
+
+    "/#{current_locale}#{path}"
+  end
+
+  def alternate_locale
+    current_locale == :nl ? :en : :nl
+  end
+
+  def alternate_locale_path
+    path = @item.path.nil? || @item.path.empty? ? '/' : @item.path
+    "/#{alternate_locale}#{path}"
+  end
+
   def t(key, **options)
-    I18n.t(key, **options)
+    I18n.t(key, **options.merge(locale: current_locale))
   end
 
   def l(object, **options)
-    I18n.l(object, **options)
+    I18n.l(object, **options.merge(locale: current_locale))
   end
 end
